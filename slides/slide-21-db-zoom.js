37775c11ae342d86f4b51326f3fb63177c71654a
Minwoo Kim <minwokim@gmarket.com>
2026-05-25 00:09:49 +0900

    Morph JSON return into DB zoom

--- slides/slide-21-db-zoom.js
+++ slides/slide-21-db-zoom.js
@@ -12,6 +12,17 @@ window.PREZI_MORPH_SLIDES[20] = {
       "w": 2.35,
       "h": 2.35,
       "z": 1
+    },
+    {
+      "key": "!!web_scene_image",
+      "name": "!!web_scene_image",
+      "descr": "JSON response returning to bot client",
+      "src": "assets/image_bot.png",
+      "x": -1.462,
+      "y": -0.675,
+      "w": 2.35,
+      "h": 2.35,
+      "z": 2
     }
   ]
 };
